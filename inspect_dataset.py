ce0bcc90efdf0411fac1c1f4dbabfffe000754d7
Vaishnav14220 <45699221+Vaishnav14220@users.noreply.github.com>
2025-11-06 09:50:48 +0100

    Remove Hugging Face and dataset files, keep only website files

--- inspect_dataset.py
+++
@@ -1,6 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset("smitathkr1/organic_reactions_structured")
-print(dataset)
-print(dataset['train'].column_names)
-print(dataset['train'][0])
